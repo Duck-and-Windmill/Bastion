ecf74414fc2635ba2c67f1046791d028991fcb86
charlesma4 <charlesma4@gmail.com>
2017-11-04 22:32:25 -0400

    woo

--- src/ML/classification.py
+++ src/ML/classification.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import scraper
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 
 def index():
@@ -17,11 +17,22 @@ def index():
 		"fashion":[]
 	}
 
-	for c in gen_categories:
-		page = requests.get("https://www.google.com/search?q=" + c)
-		soup = BeutifulSoup(page.content)
+	for cat in gen_categories:
+		page = requests.get("https://www.google.com/search?q=" + cat)
+		soup = BeautifulSoup(page.content, 'lxml')
 		links = soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)"))
 		for link in links:
-			gen_categories[c].add(scraper.find_keywords(re.split(":(?=http)",link["href"].replace("/url?q=",""))))
+			print(link)
+			url = re.split(":(?=http)",link["href"].replace("/url?q=",""))[0]
+			if url[:30] == "http://www.chicagotribune.com/":
+				continue
+			elif url[:28] == "https://www.bostonglobe.com/":
+				continue
+			keys = scraper.find_keywords(scraper.scrape(url))
+			gen_categories[cat].extend(keys)
 
+	print(gen_categories)
 	return gen_categories
+
+index()
+
