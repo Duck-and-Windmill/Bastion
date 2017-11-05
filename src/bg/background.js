c1036b0d0283d1aa3d74513e78110e6db1934d30
Rudhra Raveendran <rudytheninja@gmail.com>
2017-11-05 03:15:07 -0500

    finished extension

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,26 +1,49 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+const config = {
+      apiKey: "AIzaSyAkwuO9GxcgxBrTJDrLF7SE4lUZNnewSn0",
+      authDomain: "bastion-66cb2.firebaseapp.com",
+      databaseURL: "https://bastion-66cb2.firebaseio.com",
+      projectId: "bastion-66cb2",
+      storageBucket: "bastion-66cb2.appspot.com",
+      messagingSenderId: "1071327721533"
+    };
+firebase.initializeApp(config);
+const database = firebase.database();
 
 var oldURL = "";
+var oldTime = new Date();
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        console.log(tabs)
         var url = tabs[0].url;
         chrome.extension.getBackgroundPage().console.log("Old URL: " + oldURL);
         chrome.extension.getBackgroundPage().console.log("Current URL: " + url);
         oldURL = url;
+
+        var urlData = new URL(url);
+        var fixedString = urlData.hostname.replace(/\./g, "\\");
+        console.log(fixedString);
+
+        database.ref('hosts/' + fixedString).once('value').then(function(snapshot) {
+          var data = snapshot.val();
+          console.log(data)
+          if (data === null) {
+            data = {
+              hits: {},
+              totalHits: 0
+            }
+          }
+
+          data.totalHits += 1;
+          data.hits[(new Date()).getTime().toString()] = {
+            path: urlData.pathname,
+            timespent: (new Date() - oldTime),
+          }
+
+          database.ref('hosts/' + fixedString).set(data);
+          oldTime = new Date();
+        });
     });
   }
 });
